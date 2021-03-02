13d923bee042d642047a443861d31489506ef022
c1assic9 <65415883+c1assic9@users.noreply.github.com>
2021-03-02 20:42:36 +0530

    Update JDEMatrix.js

--- src/scripts/math/JDEMatrix.js
+++ src/scripts/math/JDEMatrix.js
@@ -14,8 +14,8 @@ export default class JDEMatrix{
         [0, 1, 0],
         [0, 0, 1]
     ];
-     const rows = this.matrix.length;
-     const columns = this.matrix[0].length;
+     const rows = this.__matrix.length;
+     const columns = this.__matrix[0].length;
     }
    function flipX() {
 		this.transform(-1, 0, 0, 0, 1 ,0 ,0 ,0 ,1);
@@ -26,28 +26,28 @@ export default class JDEMatrix{
 		return this;
 	}
   function  transpose( matA ) {
-      this.matrix = Array(this.columns).fill(0);
-      this.matrix = mat.map((r, i) => this.getColumn(i + 1));
+      this.__matrix = Array(this.columns).fill(0);
+      this.__matrix = mat.map((r, i) => this.getColumn(i + 1));
       return this;
     }
  function reset() {
-	this.a = 1;
-	this.b = 0;
-	this.c = 0;
-	this.d = 0;
-	this.e = 1;
-	this.f = 0;
-        this.h = 0;
-        this.i = 0;
-        this.j = 1;
+	this.__a = 1;
+	this.__b = 0;
+	this.__c = 0;
+	this.__d = 0;
+	this.__e = 1;
+	this.__f = 0;
+        this.__h = 0;
+        this.__i = 0;
+        this.__j = 1;
 		return this;
 	}
 function getrow(r) {
      
-      return this.matrix[r - 1].slice(0);
+      return this.__matrix[r - 1].slice(0);
     }
       function setvalue(r, c, v) {
-      this.matrix[r-1][c-1] = v;
+      this.__matrix[r-1][c-1] = v;
       return this;
     }
 function rotate(angle) {
